02550cb91fbe6d3c11564750c29b1fb49cd98b73
unknown <PERINFR@PF49D05D.cdm.cm-cic.fr>
2016-02-17 15:46:04 +0100

    Add invert exchange rate with 4 digits precision

--- CurrencyManager.UnitTest/ExchangeRateTest.cs
+++ CurrencyManager.UnitTest/ExchangeRateTest.cs
@@ -11,11 +11,28 @@ namespace CurrencyManager.UnitTest
         [TestMethod]
         public void Change_From550With1Dot2053Rate_ShouldReturn662Dot9150()
         {
-            var bank = new ExchangeRate(1.2053);
+            var exchangeRate = new ExchangeRate(1.2053);
             
-            var exchangeValue = bank.Change(550);
+            var exchangeValue = exchangeRate.Change(550);
 
             Assert.AreEqual(662.9150, exchangeValue);
+        }
+
+        [TestMethod]
+        public void InvertRate_WhenRateEqual0Dot9661_InvertRateShouldEqual1Dot0351()
+        {
+            var exchangeRate = new ExchangeRate(0.9661);
+            Assert.AreEqual(1.0351, exchangeRate.InvertRate);
+        }
+
+        [TestMethod]
+        public void ChangeInvert_From662Dot9150With0Dot9661Rate_ShouldReturn686Dot1833()
+        {
+            var exchangeRate = new ExchangeRate(0.9661);
+
+            var exchangeValue = exchangeRate.ChangeInvert(662.9150);
+
+            Assert.AreEqual(686.1833, exchangeValue);
         }
     }
 }
--- CurrencyManager/ExchangeRate.cs
+++ CurrencyManager/ExchangeRate.cs
@@ -5,16 +5,23 @@ namespace CurrencyManager
     public class ExchangeRate
     {
         private const int EXCHANGE_PRECISION = 4;
-        public double Rate { get; set; }
 
+        public double Rate { get; set; }
+        public double InvertRate { get { return Math.Round(1/Rate, EXCHANGE_PRECISION); } }
+        
         public ExchangeRate(double rate)
         {
             Rate = rate;
         }
 
-        public double Change(int valueToChange)
+        public double Change(double valueToChange)
         {
             return Math.Round(valueToChange*Rate, EXCHANGE_PRECISION);
+        }
+
+        public double ChangeInvert(double valueToChange)
+        {
+            return Math.Round(valueToChange*InvertRate, EXCHANGE_PRECISION);
         }
     }
 }
