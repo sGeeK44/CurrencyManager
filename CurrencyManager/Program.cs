c1b521494aa1a4c79e4aaed506dc021496d0dac2
unknown <PERINFR@PF49D05D.cdm.cm-cic.fr>
2016-02-17 10:50:28 +0100

    Init console project

---
+++ CurrencyManager/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CurrencyManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Appuyer sur une touche pour continuer...");
+            Console.ReadKey();
+        }
+    }
+}
