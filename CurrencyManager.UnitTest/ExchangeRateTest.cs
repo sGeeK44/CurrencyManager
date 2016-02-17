b94a7447a3c21bd0cec9ffd7d9529c3735eb5f46
unknown <PERINFR@PF49D05D.cdm.cm-cic.fr>
2016-02-17 14:45:00 +0100

    Add exchange rate with 4 digits precision

---
+++ CurrencyManager.UnitTest/ExchangeRateTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CurrencyManager.UnitTest
+{
+    /// <summary>
+    /// Summary description for CurrencyExchangerTest
+    /// </summary>
+    [TestClass]
+    public class ExchangeRateTest
+    {
+        [TestMethod]
+        public void Change_From550With1Dot2053Rate_ShouldReturn662Dot9150()
+        {
+            var bank = new ExchangeRate(1.2053);
+            
+            var exchangeValue = bank.Change(550);
+
+            Assert.AreEqual(662.9150, exchangeValue);
+        }
+    }
+}
---
+++ CurrencyManager/ExchangeRate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CurrencyManager
+{
+    public class ExchangeRate
+    {
+        private const int EXCHANGE_PRECISION = 4;
+        public double Rate { get; set; }
+
+        public ExchangeRate(double rate)
+        {
+            Rate = rate;
+        }
+
+        public double Change(int valueToChange)
+        {
+            return Math.Round(valueToChange*Rate, EXCHANGE_PRECISION);
+        }
+    }
+}
