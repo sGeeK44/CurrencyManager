438ba6c103b625847eebc17b38e4efe8b34c4bdc
sGeeK <francois_perin@hotmail.fr>
2016-02-18 15:48:04 +0100

    Add summary

--- CurrencyManager/ExchangeChainFactory.cs
+++ CurrencyManager/ExchangeChainFactory.cs
@@ -4,8 +4,15 @@ using System.Linq;
 
 namespace CurrencyManager
 {
+    /// <summary>
+    /// Expose method to build ExchangeChain
+    /// </summary>
     public class ExchangeChainFactory
     {
+        /// <summary>
+        /// Create a new factory with specified available exchange currency
+        /// </summary>
+        /// <param name="availableExchangeCurrency">All available exchange currency can be use to build chain</param>
         public ExchangeChainFactory(IList<IExchangeCurrency> availableExchangeCurrency)
         {
             if (availableExchangeCurrency == null || availableExchangeCurrency.Count == 0) throw new ArgumentNullException("availableExchangeCurrency");
@@ -13,8 +20,17 @@ namespace CurrencyManager
             AvailableExchangeCurrency = availableExchangeCurrency;
         }
 
+        /// <summary>
+        /// Get all available exchange currency that can be use to build a chain for a specified request
+        /// </summary>
         public IList<IExchangeCurrency> AvailableExchangeCurrency { get; private set; }
 
+        /// <summary>
+        /// Create a new Chain to be able to change specified currencies
+        /// </summary>
+        /// <param name="initialCurrency">Initial currency to change</param>
+        /// <param name="targetCurrency">Target currency to change</param>
+        /// <returns>Chain builded</returns>
         public IExchangeChain Create(string initialCurrency, string targetCurrency)
         {
             if (string.IsNullOrEmpty(initialCurrency)) throw new ArgumentNullException("initialCurrency");
